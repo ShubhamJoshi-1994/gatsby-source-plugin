93277a1d6b8101895af92d1e9a74dcbd38455cdf
Shubham Joshi <shubham@betalectic.com>
2020-03-17 13:34:02 +0530

    source plugin implemented

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,33 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [{
+    resolve: "gatsby-source-apiserver",
+      options: {
+        typePrefix: "apiSource__",
+        url: `https://jsonplaceholder.typicode.com/posts`,
+        method: "get",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        name: `posts`,
+        localSave: false,
+        path: `${__dirname}/src/gql/`,
+        verboseOutput: true, // For debugging purposes
+
+        // Pass an array containing any number of the entity configuration properties (except verbose, auth0Config),
+        // any not specified are defaulted to the general properties that are specified
+        // Only available from version 2.1.0
+        entitiesArray: [
+          {
+            url: `https://jsonplaceholder.typicode.com/posts`,
+            method: "get",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            name: `posts`
+          }
+        ]
+      }
+    }]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+function Index({data}) {
+  console.log('data', data);
+  return (
+    <div>
+      <p>Hello World</p>
+      <ul>
+        {data.allApiSourcePosts.edges
+        .filter(({ node }) => node.id !== "dummy")
+        .map(({ node }, index) => (
+          <li key={index}>{node.title}</li>
+        ))}
+      </ul>
+      {/* <div>
+        <button onClick={() => callApi()}>Trigger</button>
+      </div> */}
+    </div>
+  )
+}
+
+export const query = graphql`
+{
+  allApiSourcePosts {
+    edges {
+      node {
+        id
+        title
+        userId
+        body
+        alternative_id
+      }
+    }
+  }
+}
+`
+
+export default Index;
